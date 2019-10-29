e4dfa474374ecef9fc896b7479c92da2c4db9e8d
kevin <kevin.hernandez1802@gmail.com>
2019-10-29 14:45:17 -0500

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,13 @@ import Layout from '../components/Layout';
 import PageFooter from '../components/PageFooter';
 import Sidebar from "../components/Sidebar"
 
+import pic2 from '../assets/images/pic02.jpg';
+import pic3 from '../assets/images/pic03.jpg';
+import pic4 from '../assets/images/pic04.jpg';
+import pic5 from '../assets/images/pic05.jpg';
+import pic6 from '../assets/images/pic06.jpg';
+import pic7 from '../assets/images/pic07.jpg';
+import pic8 from '../assets/images/pic08.jpg';
 import Scroll from '../components/Scroll';
 
 const sections = [

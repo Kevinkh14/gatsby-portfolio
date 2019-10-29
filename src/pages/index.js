c7968a0e07ca3d3d39339387604d0f4f2090221c
kevin <kevin.hernandez1802@gmail.com>
2019-10-29 12:21:33 -0500

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import PageFooter from '../components/PageFooter';
-import SideBar from '../components/Sidebar/main';
+import SideBar from '../components/Sidebar/Index';
 
 import pic2 from '../assets/images/pic02.jpg';
 import pic3 from '../assets/images/pic03.jpg';
